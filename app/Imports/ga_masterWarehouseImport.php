df1ad0a4b87897529f7a09c0407f09e087a33dc3
Muhammad Wahyu <mwhy25@gmail.com>
2023-02-15 14:04:09 +0700

    Update ga_masterWarehouseImport.php
    Add import

--- app/Imports/ga_masterWarehouseImport.php
+++ app/Imports/ga_masterWarehouseImport.php
@@ -2,16 +2,31 @@
 
 namespace App\Imports;
 
+use Carbon\Carbon;
 use Illuminate\Support\Collection;
-use Maatwebsite\Excel\Concerns\ToCollection;
+use Illuminate\Support\Facades\DB;
+use Maatwebsite\Excel\Concerns\ToModel;
+use Maatwebsite\Excel\Concerns\WithHeadingRow;
+use Maatwebsite\Excel\Concerns\WithMultipleSheets;
+use Illuminate\Support\Str;
 
-class ga_masterWarehouseImport implements ToCollection
+class ga_masterWarehouseImport implements ToModel, WithHeadingRow, WithMultipleSheets
 {
-    /**
-    * @param Collection $collection
-    */
-    public function collection(Collection $collection)
+    public function model(array $row)
     {
-        //
+
+        DB::table('ga_masterWarehouse')->insert([
+            'uuid_gudang'=>Str::uuid(),
+            'nama_gudang'=>$row['nama_gudang'],
+            'status_gudang'=>$row['status_gudang'],
+            'created_at'=>Carbon::now(),
+            'updated_at'=>Carbon::now()
+        ]);
+    }
+    public function sheets(): array
+    {
+        return [
+            0 => $this,
+        ];
     }
 }
