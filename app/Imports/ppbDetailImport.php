65fc0fffc3efac0e7d7e2b6dd5568fff551ca486
Muhammad Wahyu <mwhy25@gmail.com>
2023-02-16 11:45:49 +0700

    Update ppbDetailImport.php
    Kalau ada data ga diisi akan di skip

--- app/Imports/ppbDetailImport.php
+++ app/Imports/ppbDetailImport.php
@@ -24,38 +24,37 @@ class ppbDetailImport implements ToModel, WithHeadingRow, WithMultipleSheets
 
     public function model(array $row)
     {
-        // dd($this->data);
-            $desk=$row['ppb_deskripsi'];
-            $pengajuan=substr($this->data,10);
+        $desk = $row['ppb_deskripsi'] ?? '';
+        $merek = $row['ppb_merek'] ?? '';
+        $qty = $row['ppb_qty'] ?? '';
+        $satuan = $row['ppb_satuan'] ?? '';
+        $tipe_barang = $row['ppb_tipe_barang'] ?? '';
+
+        if ($desk !== '' && $merek !== '' && $qty !== '' && $satuan !== '' && $tipe_barang !== '') {
+            $pengajuan = substr($this->data, 10);
+
             // Remove all non-alphanumeric characters
             $str = preg_replace('/[^a-zA-Z0-9]/', '', $desk);
+
             // Remove all spaces
             $str = str_replace(' ', '', $desk);
-            $id_barang_detail=''.$pengajuan.''.md5($str).'';
+
+            $id_barang_detail = ''.$pengajuan.''.md5($str).'';
+
             DB::table('proc_ppb_detail')->insert([
-                'id_pengajuan'=>$this->data,
-                'id_barang_detail'=>$id_barang_detail,
-                'ppb_qty'=>$row['ppb_qty'],
-                'ppb_satuan'=>$row['ppb_satuan'],
-                'ppb_deskripsi'=>$desk,
-                'ppb_tipe_barang'=>$row['ppb_tipe_barang'],
-                'ppb_merek'=>$row['ppb_merek'],
-                'ppb_pemasok_panel'=>$row['ppb_pemasok_panel'],
-                'created_at'=>Carbon::now(),
+                'id_pengajuan' => $this->data,
+                'id_barang_detail' => $id_barang_detail,
+                'ppb_qty' => $qty,
+                'ppb_satuan' => $satuan,
+                'ppb_deskripsi' => $desk,
+                'ppb_tipe_barang' => $tipe_barang,
+                'ppb_merek' => $merek,
+                'ppb_pemasok_panel' => $row['ppb_pemasok_panel'],
+                'created_at' => Carbon::now(),
             ]);
-        // }
-        // DB::table('po_panel_det')->insert([
-        //     'panel_cell'=>$row['cell'],
-        //     'panel_spv'=>$row['SPV'],
-        //     'panel_wiring'=>$row['Wiring'],
-        //     'panel_mekanik'=>$row['Mekanik'],
-        //     'panel_FW'=>$row['Jenis_F_W'],
-        //     'panel_LM'=>$row['Jenis_L_M'],
-        //     'panel_status_komponen'=>$row['Status_Komponen'],
-        //     'created_at'=>Carbon::now(),
-        //     'updated_at'=>Carbon::now()
-        // ]);
+        }
     }
+
     public function sheets(): array
     {
         return [
