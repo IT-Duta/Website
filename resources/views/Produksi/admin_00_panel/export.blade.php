755c65d62335ced00663a8f012bb2c3da3bebb17
Muhammad Wahyu <mwhy25@gmail.com>
2023-02-27 14:08:38 +0700

    update

---
+++ resources/views/Produksi/admin_00_panel/export.blade.php
@@ -0,0 +1,32 @@
+<table>
+    <thead>
+        <tr>
+            <th>No</th>
+            <th>Nomor Seri</th>
+            <th>Customer</th>
+            <th>Panel</th>
+            <th>Cell</th>
+            <th>Nama Proyek</th>
+            <th>Deadline Produksi</th>
+            <th>Deadline QC</th>
+            <th>Status Pekerjaan</th>
+            <th>Jenis Panel</th>
+            <th>Tegangan Panel</th>
+            <th>Tipe Panel</th>
+            <th>Tanggal Manufaktur</th>
+            <th>SPV</th>
+            <th>Wiring</th>
+            <th>Mekanik</th>
+            <th>Status Komponen</th>
+        </tr>
+    </thead>
+    <tbody>
+        <tr>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+        </tr>
+    </tbody>
+</table>
