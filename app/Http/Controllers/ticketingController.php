dc86bb63472aac5c560dcb88ce6b47052adaa6fc
SaktiPutraS <uzucread@gmail.com>
2023-05-04 12:14:57 +0700

    test

--- app/Http/Controllers/ticketingController.php
+++ app/Http/Controllers/ticketingController.php
@@ -15,48 +15,52 @@ use Maatwebsite\Excel\Facades\Excel;
 
 class ticketingController extends Controller
 {
-    public function index(){
-        $list=DB::table('ticketing')->orderByDesc('id')->get();
-        return view('ticketing.index',compact('list'));
+    public function index()
+    {
+        $list = DB::table('ticketing')->orderByDesc('id')->get();
+        return view('ticketing.index', compact('list'));
     }
-    public function create(){
-        $karyawan=DB::table('duta_karyawan')->orderBy('k_nama','asc')->get();
+    public function create()
+    {
+        $karyawan = DB::table('duta_karyawan')->orderBy('k_nama', 'asc')->get();
         return view('ticketing.create')->with(compact('karyawan'));
     }
-    public function store(Request $request){
-       $request->validate([
-        'ticket_user' => 'required',
-        'ticket_referrer' =>'required',
-        'ticket_type' =>'required',
-        'ticket_judul' => 'required',
-        'ticket_detail' =>'required',
-       ]);
-        $fileName ="";
-        $filePath="";
-        $imageName="";
-        $sequence=1;
-        $check = DB::table('ticketing')->orderBy('id','desc')->first();
-        if(empty($check)){
+    public function store(Request $request)
+    {
+        $request->validate([
+            'ticket_user' => 'required',
+            'ticket_referrer' => 'required',
+            'ticket_type' => 'required',
+            'ticket_judul' => 'required',
+            'ticket_detail' => 'required',
+        ]);
+        $fileName = "";
+        $filePath = "";
+        $imageName = "";
+        $sequence = 1;
+        $check = DB::table('ticketing')->orderBy('id', 'desc')->first();
+        if (empty($check)) {
             $sequence = 1;
         } else {
             $sequence = $check->urut + 1;
         }
-         // Ubah ketika ganti tahun, jika tahun sekarang tidak sama dengan tahun terakhir input maka ubah nilai ke 1
-         $currentYear = date("Y");
-         $createdAtYear = date("Y", strtotime($check->created_at));
-         if ($currentYear != $createdAtYear) {
-             $sequence=1;
-         }
+        // Ubah ketika ganti tahun, jika tahun sekarang tidak sama dengan tahun terakhir input maka ubah nilai ke 1
+        $currentYear = date("Y");
+        $createdAtYear = date("Y", strtotime($check->created_at));
+        if ($currentYear != $createdAtYear) {
+            $sequence = 1;
+        }
         $tahun = date('Y');
         $bulan = date('m');
         $number = substr(str_repeat(0, 3) . $sequence, -3);
         $numberTicket = '' . $number . '/E-TICKET/' . $bulan . '/' . $tahun . '';
-        if(!empty($request->file('file'))){
+        if (!empty($request->file('file'))) {
             $fileName = $request->file('file')->getClientOriginalName();
             // dd($request->file('file')->getClientOriginalName());
             $fileExt = pathinfo($fileName, PATHINFO_EXTENSION);
-            $imageName=$number.'_eticket_'.$bulan.'_'.$tahun.'.'.$fileExt.'';
-            $filePath = $request->file('file')->storeAs('public/images',$imageName);
+            $imageName = $number . '_eticket_' . $bulan . '_' . $tahun . '.' . $fileExt . '';
+            // $filePath = $request->file('file')->storeAs('public/images', $imageName);
+            $filePath = $request->file('file')->move(public_path('storage\images'), $imageName);
             // dd($name);
         }
         DB::table('ticketing')->insert([
@@ -71,31 +75,32 @@ class ticketingController extends Controller
             'ticket_filepath' => $filePath,
             'ticket_status' => 'Waiting'
         ]);
-            // Send Email
+        // Send Email
 
-            // Mail::mailer('IT')->to('edp@ptduta.com', 'IT Staff')->send(new newTicket);
+        // Mail::mailer('IT')->to('edp@ptduta.com', 'IT Staff')->send(new newTicket);
 
-        return redirect()->route('ticket')->with('status','Data has been submitted');
+        return redirect()->route('ticket')->with('status', 'Data has been submitted');
     }
-    public function update(Request $request){
-        $fileName ="";
+    public function update(Request $request)
+    {
+        $fileName = "";
         $request->validate([
             'id' => 'required',
-            'ticket_referrer' =>'required',
-            'ticket_type' =>'required',
+            'ticket_referrer' => 'required',
+            'ticket_type' => 'required',
             'ticket_judul' => 'required',
-            'ticket_detail' =>'required',
-            'ticket_solver' =>'required',
-            'ticket_solve' =>'required',
-            'ticket_status' =>'required',
-           ]);
-        if(!empty($request->file('ticket_file_solve'))){
+            'ticket_detail' => 'required',
+            'ticket_solver' => 'required',
+            'ticket_solve' => 'required',
+            'ticket_status' => 'required',
+        ]);
+        if (!empty($request->file('ticket_file_solve'))) {
             $fileName = $request->file('ticket_file_solve')->getClientOriginalName();
             // dd($request->file('file')->getClientOriginalName());
             // $fileName = pathinfo($fileName, PATHINFO_EXTENSION);
-            $request->file('ticket_file_solve')->storeAs('public/images',$fileName);
+            $request->file('ticket_file_solve')->storeAs('public/images', $fileName);
         }
-        DB::table('ticketing')->where('id',$request->id)->update([
+        DB::table('ticketing')->where('id', $request->id)->update([
             'ticket_judul' => $request->ticket_judul,
             'ticket_type' => $request->ticket_type,
             'ticket_referrer' => $request->ticket_referrer,
@@ -106,18 +111,20 @@ class ticketingController extends Controller
             'ticket_status' => $request->ticket_status,
             'updated_at' => Carbon::now(),
         ]);
-        return redirect()->route('ticket')->with('status','Data has been updated');
+        return redirect()->route('ticket')->with('status', 'Data has been updated');
     }
-    public function delete($id){
-        DB::table('ticketing')->where('id',$id)->delete();
-        return redirect()->back()->with('status','Data has been deleted');
+    public function delete($id)
+    {
+        DB::table('ticketing')->where('id', $id)->delete();
+        return redirect()->back()->with('status', 'Data has been deleted');
     }
-    public function view($id){
-        $list=DB::table('ticketing')->where('id',$id)->first();
+    public function view($id)
+    {
+        $list = DB::table('ticketing')->where('id', $id)->first();
         // $url = Storage::url($list->ticket_filepath);
         // dd($url);
         // return view('ticketing.view',compact('list','url'));
-        return view('ticketing.view',compact('list'));
+        return view('ticketing.view', compact('list'));
     }
     // public function download($id){
     //     $file=DB::table('ticketing')->where('id',$id)->first();
@@ -133,31 +140,31 @@ class ticketingController extends Controller
     // }
     public function download($id)
     {
-        $file=DB::table('ticketing')->where('id',$id)->first();
-        $file=$file->ticket_file_solve;
-        $filepath = public_path('storage/images/'.$file.'');
+        $file = DB::table('ticketing')->where('id', $id)->first();
+        $file = $file->ticket_file_solve;
+        $filepath = public_path('storage/images/' . $file . '');
         return Response::download($filepath);
     }
     public function export()
     {
-        $time=Carbon::now();
-        $time=date_format($time,'d-m-y, H.i.s');
-        $filename='Laporan Tiket '.$time.'.xlsx';
+        $time = Carbon::now();
+        $time = date_format($time, 'd-m-y, H.i.s');
+        $filename = 'Laporan Tiket ' . $time . '.xlsx';
         return Excel::download(new ticketExport, $filename);
     }
     public function getTitle(Request $request)
-{
-    // Get the search term from the request
-    $searchTerm = $request->input('searchTerm');
+    {
+        // Get the search term from the request
+        $searchTerm = $request->input('searchTerm');
 
-    // Query the database for search suggestions
-    $suggestions = DB::table('ticketing')
-      ->select('ticket_judul')
-      ->where('ticket_judul', 'like', "%$searchTerm%")
-      ->limit(5)
-      ->get();
+        // Query the database for search suggestions
+        $suggestions = DB::table('ticketing')
+            ->select('ticket_judul')
+            ->where('ticket_judul', 'like', "%$searchTerm%")
+            ->limit(5)
+            ->get();
 
-    // Return the search suggestions as a JSON response
-    return response()->json($suggestions);
-}
+        // Return the search suggestions as a JSON response
+        return response()->json($suggestions);
+    }
 }
