1ae7f6a27c8ee6ac126d7567639892d174e79d3e
SaktiPutraS <uzucread@gmail.com>
2023-06-21 12:03:29 +0700

    Update hard_req_Export.blade.php

--- resources/views/Form/Hardware/Permintaan/hard_req_Export.blade.php
+++ resources/views/Form/Hardware/Permintaan/hard_req_Export.blade.php
@@ -9,25 +9,25 @@
             <th>Referensi</th>
             <th>Alasan</th>
             <th>Status</th>
-            <th>Tanggal Permintaan</th>
-            <th>Tanggal Selesai</th>
+            <th>Tanggal Progress</th>
+            <th>Tanggal Finish</th>
         </tr>
     </thead>
     <tbody>
-        <?php $nomor=1?>
+        <?php $nomor = 1; ?>
         @foreach ($exportData as $list)
-        <tr>
-            <td><?php echo $nomor++; ?></td>
-            <td>{{$list->hard_req_number}}</td>
-            <td>{{$list->hard_req_user}}</td>
-            <td>{{$list->hard_req_divisi}}</td>
-            <td>{{$list->hard_req_location}}</td>
-            <td>{{$list->hard_req_referensi}}</td>
-            <td>{{$list->hard_req_alasan}}</td>
-            <td>{{$list->hard_req_status}}</td>
-            <td>{{$list->created_at}}</td>
-            <td>{{$list->updated_at}}</td>
-        </tr>
+            <tr>
+                <td><?php echo $nomor++; ?></td>
+                <td>{{ $list->hard_req_number }}</td>
+                <td>{{ $list->hard_req_user }}</td>
+                <td>{{ $list->hard_req_divisi }}</td>
+                <td>{{ $list->hard_req_location }}</td>
+                <td>{{ $list->hard_req_referensi }}</td>
+                <td>{{ $list->hard_req_alasan }}</td>
+                <td>{{ $list->hard_req_status }}</td>
+                <td>{{ $list->hard_req_progress }}</td>
+                <td>{{ $list->hard_req_finish }}</td>
+            </tr>
         @endforeach
     </tbody>
 </table>
